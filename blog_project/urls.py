62cfdfbded799d628bbd25532e587b976c8cd160
jasjdn <137282501+jasjdn@users.noreply.github.com>
2024-05-15 04:09:25 +0700

    Update urls.py

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include  # new
-from . import views
+# from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),  # new
